131ddd57fbf25e90a1c7e3ec69a8a560e39f178b
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2022-09-18 22:02:00 -0300

    category template

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
 
@@ -19,14 +19,31 @@ def shop(request):
     all_products
     """
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products
+               }
 
     return render(request, 'shop.html', context)
 
 
-def about(requests):
-    return render(requests, 'about.html')
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    context = {'product': product
+               }
+    return render(request, 'shop-single.html', context)
 
 
-def contact(requests):
-    return render(requests, 'contact.html')
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.objects.filter(category=category)
+    context = {'category': category,
+               'products': products
+               }
+    return render(request, 'category.html', context)
