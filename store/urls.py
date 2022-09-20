9b287d5f76d83a0c1dc44b0638a72b2e842f7e41
Helton Teixeira de Souza <helton.souza@ongame.corp.com.br>
2022-09-20 10:11:58 -0300

    adding a default image to products

--- store/urls.py
+++ store/urls.py
@@ -9,8 +9,8 @@ app_name = 'store'
 urlpatterns = [
     path('', views.home, name='home'),
     path('shop', views.shop, name='shop'),
-    path('item/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('search/<slug:category_slug>/', views.category_list, name='category_list'),
+    path('item/<slug:slug>', views.product_detail, name='product_detail'),
+    path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
 
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
