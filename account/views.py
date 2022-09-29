12c6fe4582c37cebc550edebe6ad1bdcfdc8f98f
Helton Teixeira de Souza <helton.souza@ongame.corp.com.br>
2022-09-29 12:06:40 -0300

    start building signup

---
+++ account/views.py
@@ -0,0 +1,37 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from .forms import RegistrationForm
+from .token import account_activation_token
+
+
+def account_register(request):
+
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            cd = registerForm.cleaned_data
+            user.email = cd['email']
+            user.set_password(cd['password'])
+            user.is_active = False
+            user.save()
+            # Setup email
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject=subject, message=message)
+    else:
+        registerForm = RegistrationForm()
+        return render(request, 'account/registration/register.html', {'form': registerForm})
