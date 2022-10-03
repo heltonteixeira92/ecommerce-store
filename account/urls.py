1c6414597514d42d407dc97ebceead131d9b2ce8
Helton Teixeira de Souza <helton.souza@ongame.corp.com.br>
2022-10-03 14:45:31 -0300

    edit/delete user

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 from .forms import UserLoginForm
@@ -12,5 +13,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='/account/login/'), name='logout'),
     path('register/', views.account_register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>)/', views.account_activate, name='activate'),
+    # User dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('profile/edit/', views.edit_details, name='edit_details'),
+    path('profile/delete-user/', views.delete_user, name='delete_user'),
+    path('profile/delete-confirm/', TemplateView.as_view(template_name='user/delete_confirm.html'),
+         name='delete_confirmation'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
@@ -7,9 +7,36 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserEditForm
 from .models import UserBase
 from .token import account_activation_token
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'user/dashboard.html')
+
+
+@login_required
+def edit_details(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+
+    return render(request, 'user/edit_details.html', {'user_form': user_form})
+
+
+@login_required
+def delete_user(request):
+    user = UserBase.objects.get(user_name=request.user)
+    user.is_active = False
+    user.save()
+    logout(request)
+    return redirect('account:delete_confirmation')
 
 
 def account_register(request):
@@ -56,8 +83,3 @@ def account_activate(request, uidb64, token):
         return redirect('account:dashboard')
     else:
         return render(request, 'registration/activation_invalid.html')
-
-
-@login_required
-def dashboard(request):
-    return render(request, 'user/dashboard.html')
